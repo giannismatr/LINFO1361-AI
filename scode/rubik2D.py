a7f06aa03c462805a2d32ebfcd45d278cfac0ba1
giannismatr <giannismatrozos98@gmail.com>
2022-02-24 17:21:09 +0100

    finished results

--- scode/rubik2D.py
+++ scode/rubik2D.py
@@ -28,19 +28,19 @@ class Rubik2D(Problem):
 
     def result(self, state, action):
         my_test_grid=state.grid
-        matrix = []
-        if action[1]=="right":
-            
+        if action[1]=="right":      
             for x in state.grid[action[0]]:
                 pos=int(state.shape[0]-(int(x)+1))
                 if (state.grid[action[0]].index(x)+action[2])<=state.shape[0]:
                     my_test_grid[action[0]][int(x)+action[2]]=state.grid[action[0]][int(x)]
                 else:
-                    my_test_grid[action[0][0+pos-1]]=str(state.grid[action[0][x]])
+                    my_test_grid[action[0][0+pos-1]]=state.grid[action[0]][x]
         else:
             for y in state.grid[action[0]]:
                 if (state.grid[action[0]].index(y)+action[2])<=state.shape[1]:
-                    pass
+                    my_test_grid[int(y)+action[2]][action[0]]=state.grid[int(y)][action(0)]
+                else:
+                    my_test_grid[action[0+pos-1]][action[0]]=state.grid[y][action[0]]
         return my_test_grid
 
     def goal_test(self, state):
